a9b82b303b06bb2e6dc136f264602547a41820b8
Kamil Świerad <kam.swierad@gmail.com>
2017-10-16 02:18:10 +0200

    All of the tasks

--- src/src/agh/cs/lab2/Position.java
+++ src/src/agh/cs/lab2/Position.java
@@ -17,23 +17,24 @@ public class Position {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String result;
         result = String.format("(%d,%d)", this.x, this.y);
         return result;
     }
 
-    public boolean smaller(Position operand){
+    public boolean smaller(Position operand) {
         return this.x < operand.x && this.y < operand.y;
     }
 
-    public boolean larger(Position operand){
+    public boolean larger(Position operand) {
         return this.x > operand.x && this.y > operand.y;
     }
 
-    public Position add(Position operand){
+    public Position add(Position operand) {
         return new Position(this.x + operand.x, this.y + operand.y);
     }
+
 
     @Override
     public boolean equals(Object o) {
@@ -43,17 +44,13 @@ public class Position {
         Position position = (Position) o;
 
         if (x != position.x) return false;
-        if (y != position.y) return false;
-        return equals == position.equals;
+        return y == position.y;
     }
 
     @Override
     public int hashCode() {
         int result = x;
         result = 31 * result + y;
-        result = 31 * result + (equals ? 1 : 0);
         return result;
     }
-
-    public boolean equals
 }
